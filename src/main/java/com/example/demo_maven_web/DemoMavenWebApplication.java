bb464099dffdb22b14b727224fa834e7f266edc0
khg <kang.hyungoo@gmail.com>
2025-05-25 18:31:36 +0900

    2025-05-25 2

--- src/main/java/com/example/demo_maven_web/DemoMavenWebApplication.java
+++ src/main/java/com/example/demo_maven_web/DemoMavenWebApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo_maven_web;
 
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +16,19 @@ public class DemoMavenWebApplication {
 
 	public static void main(String[] args) {
 
+
 		SpringApplication.run(DemoMavenWebApplication.class, args);
+
+
+		//SpringApplication app = new SpringApplication(DemoMavenWebApplication.class);
+		//app.setBannerMode(Banner.Mode.OFF);
+		//app.run(args);
+
+		//아래는 실행 안됨
+//		new SpringApplicationBuilder()
+//				.sources(SpringinitApplication.class)
+//				//.main(SpringApplication.class)
+//				.run(args);
 
 
 	}
